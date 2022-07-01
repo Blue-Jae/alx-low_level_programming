71d4dca1118b0b8bc380eb8d693907f9687b784a
Blue-Jae <jaemeelahali@gmail.com>
2022-07-01 08:43:29 -0700

    adding 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 
 /*
- * main - Determines if a number is positive, negative or zero.
+ * main - Determines if a number is positive, negative or zero. * main - Positive or Negative - print sign of random number
+ * @void: Empty parameter list for main.
+ * 
+ * Description: this if/else statement delares the sign (positive
+ * or negative) of a random number. It also states if the value
+ * is zero.
  *
  * Return: Always 0 (Success)
  */
