1ff4ad568ea12fd8d75279340f68fac3609b8463
Blue-Jae <jaemeelahali@gmail.com>
2022-07-07 15:24:19 -0700

    adding 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,24 @@ void fizz_buzz(void)
 
 	for (x = 1; x < 100; x++)
 	{
-		if (x == 100)
+		if (x % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		} else if (x % 5 == 0 && x % 3 != 0)
+		{
 			printf("Buzz");
-		else if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz ");
-		else if (x % 3 == 0)
-			printf("Fizz ");
-		else if (x % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d ", x);
+		} else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (x == 1)
+		{
+			printf("%d", x);
+		} else
+		{
+			printf(" %d", x);
+		}
 	}
 	printf("\n");
+
+	return (0);
 }
