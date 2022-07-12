3f7caf759dda3813a61ae76b8a592fc5552938ec
Blue-Jae <jaemeelahali@gmail.com>
2022-07-12 06:42:15 -0700

    add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int digit = 0;
+
+	while (digit >= 0)
+	{
+		if (str[digit] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[digit]);
+		digit++;
+	}
+}
